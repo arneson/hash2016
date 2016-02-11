491c501e7e3d49bffe97dfb8af17408a03d8c765
Joakim Berntsson <jcberntsson@gmail.com>
2016-02-11 20:43:22 +0100

    Pseudo

---
+++ src/algo/Algo.java
@@ -0,0 +1,31 @@
+package algo;
+
+/**
+ * Package: algo
+ * Author: Joakim
+ * Date: 2016-02-11 - 20:22
+ * Purpose:
+ */
+public class Algo {
+
+    // Take in data
+    // Sort orders with the order with least amount of products first
+    // While not done
+        // Get the first orders to process 33 33 33 1
+        // Combine products into a sorted list with most common product first
+        // While not done with products
+            // Cycle through warehouses with drones
+                // Take as many products as we can from list
+                // Calculate which locations we can supply with these products
+                // Calculate dijkstra path among the locations with a warehouse as ending
+                // Calculate best = #products/pathLength
+            // Take the choice with smallest best
+            // Send that drone (decrease necessary values)
+
+
+
+
+
+
+
+}
